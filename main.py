95dc7b1a5e573beff88b46bfcea0c767081e497d
dark <darkissummer@yandex.ru>
2021-11-22 19:42:42 +0300

    Not all lang

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from googletrans import *
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['post', 'get'])
+def trans():
+    code_dict = {'English': 'en', 'Spanish': 'es', "German": 'de', 'Russian': 'ru', "French": 'fr', "Kazakhstan": 'kz'}
+    translator = Translator()
+    if request.method == 'POST':
+        lang = request.form.get('lang')
+        text = request.form.get('text')
+        message = attention = ''
+        if lang == '' or lang not in code_dict.keys():
+            attention = 'Incorrect language, try again.'
+        if text == '':
+            message = 'Incorrect text, try again.'
+        if lang in code_dict.keys():
+            to_lang = code_dict[lang]
+            translation = translator.translate(text, dest=to_lang)
+            message = translation.text
+
+        return render_template('index.html', message=message, attention=attention)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    translator = Translator()
+    translation = translator.translate('HI', dest='ru')
+    translated = translation.text
+    print(translated,translation)
+    app.run()
---
+++ readme.md
@@ -0,0 +1,12 @@
+In the first input field you should type the language you want to translate your text to.
+In the second input field you should type your message for trabslation.
+Supported languages - English, French, German, Spanish, Russian.
+
+
+
+В первом поле (Type translation language) требуется ввести язык, на который необходимо перевести текст.
+Во втором поле (Type your message) необходимо ввести текст для перевода на выбранный язык.
+Поддерживаемые языки - English(Английский), French(Французский), German(немецкий), Spanish(Испанский), Russian(Русский).
+
+
+
